151bd46b292e9705acf1eeacac3ac643d2d66818
Chris Mawdesley <chris.mawdesley@gluckgames.com>
2021-02-24 23:20:43 +0000

    generic adc script

--- config.js
+++ config.js
@@ -1,15 +1,27 @@
 module.exports = [
     {
         sensor: "light",
-        cmd: "./read/light.py"
+        cmd: "./read/adc.py 7"
     },
     {
         sensor: "humidity",
         cmd: "./read/humidity.py"
     },
     {
-        sensor: "soil",
-        cmd: "./read/soil.py"
+        sensor: "soil 1",
+        cmd: "./read/adc.py 8"
+    },
+    {
+        sensor: "soil 2",
+        cmd: "./read/adc.py 10"
+    },
+    {
+        sensor: "soil 3",
+        cmd: "./read/adc.py 12"
+    },
+    {
+        sensor: "soil 4",
+        cmd: "./read/adc.py 14"
     },
     {
         sensor: "temperature",
--- read/adc.py
+++ read/adc.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import time
+import sys
 import ioexpander as io
 
 ioe = io.IOE(i2c_addr=0x18)
 
 ioe.set_adc_vref(100) 
-pin = 14
+pin = int(sys.argv[1])
 ioe.set_mode(pin, io.ADC)
 
 
