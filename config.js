90021dfe6ec6ab635c1007fa578bd3e70f50c130
Chris Mawdesley <chris.mawdesley@gluckgames.com>
2021-08-29 23:13:29 +0100

    config for zero

--- config.js
+++ config.js
@@ -7,26 +7,14 @@ module.exports = {
     sensors: [
         {
             sensor: "light",
-            cmd: "./read/adc.py 7"
+            cmd: "./read/adc.py 8"
         },
         {
             sensor: "humidity",
             cmd: "./read/humidity.py"
         },
         {
-            sensor: "soil 1",
-            cmd: "./read/adc.py 8"
-        },
-        {
-            sensor: "soil 2",
-            cmd: "./read/adc.py 10"
-        },
-        {
-            sensor: "soil 3",
-            cmd: "./read/adc.py 12"
-        },
-        {
-            sensor: "soil 4",
+            sensor: "soil",
             cmd: "./read/adc.py 14"
         },
         {
