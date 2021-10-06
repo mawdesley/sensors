cfd5cb8acc5b01b071d8c63033ba57e558a09ae4
Chris Mawdesley <chris.mawdesley@gluckgames.com>
2021-10-06 23:06:36 +0100

    ir temp read

---
+++ read/ir-object-temperature.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import board
+import busio as io
+import adafruit_mlx90614
+
+i2c = io.I2C(board.SCL, board.SDA, frequency=100000)
+
+print(adafruit_mlx90614.MLX90614(i2c).object_temperature)
