ea12df0e7ed4a58a54a5c6505720f7e21d77b247
mawdesley <christopher.mawdesley@gmail.com>
2024-06-30 12:24:57 +0100

    out of scope bug, silly

--- actionProcessor.js
+++ actionProcessor.js
@@ -8,21 +8,21 @@ const collectData = async (dataFns) => {
     return data;
 };
 
-const processAction = async ({ action, data }) => {
-    const collectedData = await collectData(data);
-    console.log("collected data", collectedData);
-    const followups = await action(collectedData);
-    console.log("followups", followups);
-
-    for (const followup of followups || []) {
-        await processAction(actions[followup]);
-    }
-}
-
 module.exports = ({ actions }) => {
+    const processAction = async ({ action, data }) => {
+        const collectedData = await collectData(data);
+        console.log("collected data", collectedData);
+        const followups = await action(collectedData);
+        console.log("followups", followups);
+
+        for (const followup of followups || []) {
+            await processAction(actions[followup]);
+        }
+    }
+
     Object.values(actions)
         .filter(action => action.interval)
         .map(action => {
             setInterval(() => processAction(action), action.interval);
         });
-};;
+};
